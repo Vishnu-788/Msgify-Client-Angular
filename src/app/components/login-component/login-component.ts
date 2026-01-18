cccfe54fa85a8b1f57559522fec595f4a9e61fd7
Vishnu Prasad <prasadvishnu788@gmail.com>
2026-01-18 18:56:38 +0530

    Validation for both of the auth pages are complete.

--- src/app/components/login-component/login-component.ts
+++ src/app/components/login-component/login-component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login-component.html',
   styleUrl: './login-component.scss',
 })
 export class LoginComponent {
-
+  loginForm: FormGroup;
+  constructor() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.minLength(5), Validators.pattern(/^\w+$/)]),
+      password: new FormControl('', [Validators.required])
+    })
+  }
+  handleLogin(){
+    if(this.loginForm.invalid){
+      console.log("Error")
+    } else {
+      this.loginForm.reset();
+    }
+  }
 }
--- src/app/components/signup-component/signup-component.ts
+++ src/app/components/signup-component/signup-component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-signup-component',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './signup-component.html',
   styleUrl: './signup-component.scss',
 })
 export class SignupComponent {
+  signupForm: FormGroup;
+
+  constructor() {
+    this.signupForm = new FormGroup({
+      username: new FormControl('',
+        [Validators.required, Validators.minLength(5), Validators.pattern(/^\w+$/)]
+      ),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  onSubmit = () => {
+    if(this.signupForm.invalid){
+      console.log("Invalid form.")
+    } else {
+      this.signupForm.reset();
+    }
+  }
 
 }
